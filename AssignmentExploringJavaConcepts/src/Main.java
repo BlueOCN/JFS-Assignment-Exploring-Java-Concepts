9d4f8bc1028b293b61a7a84455d58bea68ea9a60
BlueOCN <robgonzalezmu@gmail.com>
2025-04-21 21:42:56 -0600

    Updated Part 2 of the Assignment.

--- AssignmentExploringJavaConcepts/src/Main.java
+++ AssignmentExploringJavaConcepts/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Part 1: Exploring Variables and Primitive Types
@@ -8,10 +6,34 @@ public class Main {
         char initial = 'R';
         boolean isStudent = false;
 
+        System.out.println("Print variable values");
         System.out.println("Age: " + age);
         System.out.println("Height: " + height);
         System.out.println("Initial: " + initial);
         System.out.println("isStudent: " + isStudent);
         System.out.println();
+
+
+        //TIP Part 2: Increment and Decrement Operations
+        int counter = 10;
+
+        System.out.println("Perform increment and decrement operations");
+        counter++;
+        System.out.println("Counter: " + counter);
+        counter--;
+        System.out.println("Counter: " + counter);
+
+        System.out.println();
+        System.out.println("Count from 10 to 15");
+        for (;counter < 16; counter++) {
+            System.out.println("Counter: " + counter);
+        }
+
+        System.out.println();
+        System.out.println("Count from 15 to 10");
+        while (counter > 10) {
+            counter--;
+            System.out.println("Counter: " + counter);
+        }
     }
 }
