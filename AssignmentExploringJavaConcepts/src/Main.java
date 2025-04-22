54079f5856463d65a509a212a7b2786a300fb4ab
BlueOCN <robgonzalezmu@gmail.com>
2025-04-21 21:13:58 -0600

    Updated Part 1 of the Assignment.

--- AssignmentExploringJavaConcepts/src/Main.java
+++ AssignmentExploringJavaConcepts/src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        //TIP Part 1: Exploring Variables and Primitive Types
+        int age = 26;
+        double height = 68.8976378;
+        char initial = 'R';
+        boolean isStudent = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Initial: " + initial);
+        System.out.println("isStudent: " + isStudent);
+        System.out.println();
     }
 }
